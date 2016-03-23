c20d008b8eb10ae154f3b19ddf25951b7cebc866
Mike Bywater <mike.bywater@aptsolutions.net>
2016-03-23 19:50:04 +0000

    Use flatly theme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,12 +12,12 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
 
     <!-- Styles -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/flatly/bootstrap.min.css" rel="stylesheet">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
         body {
-            font-family: 'Lato';
+            font-family: 'Lato'; 
         }
 
         .fa-btn {
