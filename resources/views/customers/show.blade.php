8d24858c8e8b6f988271210e0aefe9c1af6df585
Mike Bywater <mike.bywater@aptsolutions.net>
2016-03-27 23:58:55 +0100

    Change heading in customer show blade

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Customer</div>
+                <div class="panel-heading">{{ $customer->forename }} {{ $customer->surname }}</div>
 
                 <div class="panel-body">
                     <div class="table-responsive">
