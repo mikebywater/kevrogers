3373913cd6b2e4aea08642cae330e1ba02ec2bd0
Mike Bywater <mike.bywater@aptsolutions.net>
2016-03-29 00:25:05 +0100

    Start of calculation javascript

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+/* Calculator
+*
+* Convert JSON to inputs add them to a div with class json-inputs
+*
+*/
+
+function jsonToInputs(json)
+{
+	var len = json.inputs.length
+	$('#json-inputs').html('')
+
+	for(i=0 ; i < len; i++)
+	{
+		$('#json-inputs').append("<input value='" + json.inputs[i] + "'>")
+	}
+} 
+
+function inputsToJson()
+{
+  return $('#json-inputs').serialize()
+}
