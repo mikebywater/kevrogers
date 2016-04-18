b970b18f5f519322edabf258791d406505e3cf5b
Mike Bywater <mike.bywater@aptsolutions.net>
2016-04-18 01:22:15 +0100

    Final aesthetic changes to search fixes #24

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -10,31 +10,38 @@
                     Customers 
                 </div>
                 <div class="panel-body">
-                    <a href="#search" class="btn btn-primary btn-sm pull-right">Search</a>   
-                    <div class="table">
-                        <table class="table table-bordered table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th>Customer Number</th><th>Surname</th><th>Forename</th><th>Address</th><th>Postcode</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            @foreach($customers as $item)
-                                <tr>
-                                    <td>
-                                        <a href="{{ url('customers', $item->id) }}">{{ $item->ref}}</a>
-                                    </td>
-                                    <td>{{ $item->surname }}</td>
-                                    <td>{{ $item->forename }}</td>
-                                    <td>{{ $item->house }} {{ $item->street }}</td>
-                                    <td>{{ $item->postcode }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                        <div class="pagination"> {!! $customers->appends(Request::only('q'))->render() !!} </div>
-                        <a href="{{ url('customers/create') }}" class="btn btn-primary pull-right btn-sm">Add New Customer</a>
-                    </div>
+                    <a href="#search" class="btn btn-primary btn-sm "><i class="fa fa-search"></i>  Search</a>
+                    <a href="/customers" class="btn btn-primary btn-sm "><i class="fa fa-close"></i>  Clear Criteria</a>
+                    <a href="{{ url('customers/create') }}" class="btn btn-primary  btn-sm"><i class="fa fa-user-plus"></i>  Add Customer</a>
+                    <br>
+                    <br>
+                    @if(!$customers->isEmpty())                        
+                       <div class="table">
+                            <table class="table table-bordered table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Customer Number</th><th>Surname</th><th>Forename</th><th>Address</th><th>Postcode</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($customers as $item)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ url('customers', $item->id) }}">{{ $item->ref}}</a>
+                                        </td>
+                                        <td>{{ $item->surname }}</td>
+                                        <td>{{ $item->forename }}</td>
+                                        <td>{{ $item->house }} {{ $item->street }}</td>
+                                        <td>{{ $item->postcode }}</td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <div class="pagination"> {!! $customers->appends(Request::only('q'))->render() !!} </div>
+                        </div>
+                    @else
+                        <h4>No Customers Match the Criteria</h4>
+                    @endif
                 </div>
             </div>
         </div>
