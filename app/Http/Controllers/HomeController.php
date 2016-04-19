e87b1751914befa7a17e37abf1dd610a1a322a31
Mike Bywater <mike.bywater@aptsolutions.net>
2016-04-20 00:25:18 +0100

    Dashboard work

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use App\Http\Requests;
+use App\Invoice;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +26,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $customers = Customer::count();
+        $outstanding = Invoice::count();
+        $monthlyIncome = Invoice::count();
+        $monthlyProjected = Invoice::count();
+        return view('home' , ['customers' => $customers, 'oustanding' => $outstanding, 'monthlyIncome' => $monthlyIncome, 'monthlyProjected' => $monthlyProjected]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,28 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+        <div class="col-md-4">
+          <div class="circle-tile ">
+            <a href="#"><div class="circle-tile-heading dark-blue"><i class="fa fa-users fa-fw fa-3x"></i></div></a>
+            <div class="circle-tile-content dark-blue">
+              <div class="circle-tile-description text-faded">Customers</div>
+              <div class="circle-tile-number text-faded ">{{$customers}}</div>
+              <a class="circle-tile-footer" href="/customers">More Info <i class="fa fa-chevron-circle-right"></i></a>
             </div>
+          </div>
         </div>
-    </div>
+         
+        <div class="col-md-4">
+          <div class="circle-tile ">
+            <a href="#"><div class="circle-tile-heading red"><i class="fa fa-gbp fa-fw fa-3x"></i></div></a>
+            <div class="circle-tile-content red">
+              <div class="circle-tile-description text-faded"> Invoices Outstanding </div>
+              <div class="circle-tile-number text-faded ">10</div>
+              <a class="circle-tile-footer" href="/invoices">More Info <i class="fa fa-chevron-circle-right"></i></a>
+            </div>
+          </div>
+        </div> 
+      </div> 
+    </div>  
 </div>
 @endsection
